f3aec5b750e06b62d8755c806915d38511a29a7f
xrSanya <grigorjew.shura2011@yandex.ru>
2020-12-24 23:30:06 +0300

    Снизил максимальную дистанцию отрисовки лодов, 3.0 не иммет смысла, только снижает фпс

--- src/Layers/xrRender/xrRender_console.cpp
+++ src/Layers/xrRender/xrRender_console.cpp
@@ -775,7 +775,7 @@ void xrRender_initconsole()
 
     Fvector tw_min, tw_max;
 
-    CMD4(CCC_Float, "r__geometry_lod", &ps_r__LOD, 0.1f, 3.f); //AVO: extended from 1.2f to 3.f
+    CMD4(CCC_Float, "r__geometry_lod", &ps_r__LOD, 0.1f, 1.2f);
     //CMD4(CCC_Float, "r__geometry_lod_pow", &ps_r__LOD_Power, 0, 2);
 
     CMD4(CCC_Float, "r__detail_density", &ps_current_detail_density, 0.3f, 1.0f); //AVO: extended from 0.2f to 0.04f and replaced variable
